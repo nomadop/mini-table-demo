e23fb41ba1ca1362965bca3508a03f932e2c617d
ljcheng <ljcheng@thoughtworks.com>
2021-04-07 15:55:16 +0800

    fix lint

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,7 +1,5 @@
 // index.ts
 // 获取应用实例
-const app = getApp<IAppOption>()
-
 Page({
   data: {
     motto: 'Hello World',
@@ -15,14 +13,14 @@ Page({
   bindViewTap() {
     wx.navigateTo({
       url: '../logs/logs',
-    })
+    });
   },
   onLoad() {
     // @ts-ignore
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
-      })
+      });
     }
   },
   getUserProfile() {
@@ -30,20 +28,20 @@ Page({
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res)
+        console.log(res);
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
-        })
+        });
       }
-    })
+    });
   },
   getUserInfo(e: any) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+    console.log(e);
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
-    })
+    });
   }
-})
+});
