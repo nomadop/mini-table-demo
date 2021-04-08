aeee7edf4d641a425cef3ae1989116a64ea1f670
ljcheng <ljcheng@thoughtworks.com>
2021-04-08 12:33:24 +0800

    fix height calculate bug

--- miniprogram/components/table/table.ts
+++ miniprogram/components/table/table.ts
@@ -29,6 +29,7 @@ Component({
     columnWidths: [] as string[],
     rowHeights: [] as string[],
     headerHeight: 'auto',
+    bodyHeight: 'auto',
     bodyWidth: 'max-content',
     extraRows: 0,
     extraColumns: 0,
@@ -42,10 +43,12 @@ Component({
       this.setData({
         columnWidths: Array(this.properties.columns.length).fill('auto'),
         rowHeights: Array(this.properties.rowData.length).fill('auto'),
+        bodyHeight: `${this.properties.height - 40}rpx`,
       });
     },
     ready(): void {
       const selector = this.createSelectorQuery();
+      selector.select('.table-container').fields({ computedStyle: ['height'] });
       selector.select('#static-cell').fields({ computedStyle: ['width', 'height'] });
       selector.select('.table-body-viewport').fields({ computedStyle: ['width', 'height'] });
       selector.selectAll('.table-header-container .table-cell').fields({ computedStyle: ['width', 'height'] });
@@ -53,8 +56,9 @@ Component({
       selector.selectAll('.table-body-container .table-cell').fields({ computedStyle: ['width'] });
       selector.select('.table-body-viewport').node();
       selector.select('.table-body-horizontal-viewport').node();
-      selector.exec((result: [SizeResult, SizeResult, SizeResult[], HeightResult[], WidthResult[], ScrollViewResult, ScrollViewResult]) => {
+      selector.exec((result: [HeightResult, SizeResult, SizeResult, SizeResult[], HeightResult[], WidthResult[], ScrollViewResult, ScrollViewResult]) => {
         const [
+          containerHeightResult,
           staticCellSizeResult,
           bodyViewportSizeResult,
           headerCellSizeResults,
@@ -69,13 +73,15 @@ Component({
         const rowHeights = rowHeightResults.map(res => res.height);
         const extraRows = this.getExtraCells(bodyViewportSizeResult.height, rowHeights, baseHeight);
         const extraColumns = this.getExtraCells(bodyViewportSizeResult.width, columnWidths, baseWidth);
-        const bodyWidth = sumSize(columnWidths) + extraColumns * parseFloat(baseWidth) + 'px';
+        const bodyWidth = `${sumSize(columnWidths) + extraColumns * parseFloat(baseWidth)}px`;
+        const bodyHeight = `${parseFloat(containerHeightResult.height) - parseFloat(maxHeaderHeightRes.height)}px`;
         this.setViewportBounces(bodyViewport, bodyHorizontalViewport);
         this.setData({
           visibility: 'visible',
           baseWidth,
           baseHeight,
           headerHeight: maxHeaderHeightRes.height,
+          bodyHeight,
           rowHeights,
           columnWidths,
           bodyWidth,
@@ -99,13 +105,13 @@ Component({
       });
       return columnWidths;
     },
-    getExtraCells: function (bodyViewportSize: string, sizes: string[], baseSize: string) {
+    getExtraCells(bodyViewportSize: string, sizes: string[], baseSize: string) {
       const rowHeightsSum: number = sumSize(sizes);
       const deltaHeight = parseFloat(bodyViewportSize) - rowHeightsSum;
       return deltaHeight > 0 ? Math.ceil(deltaHeight / parseFloat(baseSize)) : 0;
     },
-    setViewportBounces: function (...viewports: ScrollViewResult[]) {
+    setViewportBounces(...viewports: ScrollViewResult[]) {
       viewports.forEach(viewport => viewport.node.bounces = false);
-    }
+    },
   },
 });
