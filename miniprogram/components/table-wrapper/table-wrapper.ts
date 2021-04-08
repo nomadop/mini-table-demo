fe717ed03b6817b166ae5bbbcd5771af62aa7252
ljcheng <ljcheng@thoughtworks.com>
2021-04-08 18:02:46 +0800

    add table wrapper

---
+++ miniprogram/components/table-wrapper/table-wrapper.ts
@@ -0,0 +1,38 @@
+Component({
+  options: {
+    virtualHost: true,
+    pureDataPattern: /^_/,
+  },
+  properties: {
+    height: {
+      type: Number,
+      value: 300,
+    },
+    columns: {
+      type: Array,
+      value: [],
+    },
+    rowData: {
+      type: Array,
+      value: [],
+    },
+  },
+  data: {
+    _isAttached: false,
+    renderState: 0,
+  },
+  observers: {
+    "height, columns, rowData": function () {
+      if (this.data._isAttached) {
+        this.setData({
+          renderState: this.data.renderState ^ 1,
+        });
+      }
+    },
+  },
+  lifetimes: {
+    attached(): void {
+      this.setData({ _isAttached: true });
+    }
+  }
+});
